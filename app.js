88b32f8df05b5d52663749fad6bc71c455105038
BigMoneyBones <87784559+BigMoneyBones@users.noreply.github.com>
2022-09-11 14:42:11 -0500

    fully functional star counter

---
+++ app.js
@@ -0,0 +1,39 @@
+let count = 5;
+
+const add = document.getElementById("add");
+const subtract = document.getElementById("subtract");
+const counter = document.getElementById("counter");
+const star = document.querySelector(".star");
+
+const starChange = () => {
+  star.innerHTML = "";
+  for (let i = 1; i <= count; i++) {
+    const newStarDiv = document.createElement("div");
+    const newStar = document.createElement("img");
+    newStar.classList.add("star");
+    newStar.src =
+      "https://ih1.redbubble.net/image.1195355547.7815/st,small,507x507-pad,600x600,f8f8f8.jpg";
+    newStarDiv.appendChild(newStar);
+    star.append(newStarDiv);
+  }
+};
+
+const numberChange = () => {
+  counter.textContent = count;
+  starChange();
+};
+numberChange();
+
+subtract.addEventListener("click", () => {
+  if (count > 0) {
+    count -= 1;
+    numberChange();
+  }
+});
+
+add.addEventListener("click", () => {
+  if (count <= 4) {
+    count += 1;
+    numberChange();
+  }
+});
